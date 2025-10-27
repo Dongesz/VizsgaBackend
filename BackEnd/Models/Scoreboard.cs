0fce1c72be6ed2d7c81769ea70dbbeb1cf02a8c2
Dongesz <majormate12@gmail.com>
2025-10-27 18:40:27 +0100

    feat: Use Saffold command
    - use scaffold command to create models, and dbcontext file based on
      existing database

---
+++ BackEnd/Models/Scoreboard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BackEnd.Models;
+
+public partial class Scoreboard
+{
+    public ulong UserId { get; set; }
+
+    public ulong? TotalScore { get; set; }
+
+    public uint? Wins { get; set; }
+
+    public uint? Rounds { get; set; }
+
+    public DateTime? LastUpdated { get; set; }
+
+    public virtual User User { get; set; } = null!;
+}
---
+++ BackEnd/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BackEnd.Models;
+
+public partial class User
+{
+    public ulong Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public byte[] PasswordHash { get; set; } = null!;
+
+    public string UserType { get; set; } = null!;
+
+    public DateTime? CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public virtual Scoreboard? Scoreboard { get; set; }
+}

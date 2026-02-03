683462974d22d4382ebba6eaa31697b6303e061d
Skrisi23 <somikrisi@gmail.com>
2026-02-03 11:10:54 +0100

    fix: add cors to auth api

--- AuthApi/RoleBasedAuth/Program.cs
+++ AuthApi/RoleBasedAuth/Program.cs
@@ -18,6 +18,16 @@ namespace RoleBasedAuth
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader();
+                });
+            });
+
             // Add services to the container.
 
             builder.Services.AddControllers();
